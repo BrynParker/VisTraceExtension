0359f06563679830233414867fcac06347db0d8d
Derpius <49565664+Derpius@users.noreply.github.com>
2022-09-20 15:25:15 +0100

    Add comments
    Fix formatting
    
    Remove license
    
    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # VisTrace Extension Template
 
 Template repository for building a VisTrace extension
+
+Change the project name in `CMakeLists.txt` and the extension name in `source/Main.cpp`
--- source/Main.cpp
+++ source/Main.cpp
@@ -3,6 +3,7 @@
 using namespace GarrysMod::Lua;
 using namespace VisTrace;
 
+// Called by VisTrace when everything is loaded (Change the extension name to something unique)
 VISTRACE_EXTENSION_OPEN(ExampleExtension)
 {
 	LUA->PushSpecial(SPECIAL_GLOB);
@@ -11,7 +12,9 @@ VISTRACE_EXTENSION_OPEN(ExampleExtension)
 	LUA->Call(1, 0);
 }
 
-VISTRACE_EXTENSION_CLOSE() {
+// Called when the module is unloaded by GMod
+VISTRACE_EXTENSION_CLOSE()
+{
 	LUA->PushSpecial(SPECIAL_GLOB);
 	LUA->GetField(-1, "print");
 	LUA->PushString("Extension unloaded");
